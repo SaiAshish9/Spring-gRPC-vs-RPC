5a8691b08324ef92f52a10cece7de566651592ef
SaiAshish <saiashish3760@gmail.com>
2023-03-11 02:12:05 +0530

    Add files via upload

---
+++ src/test/java/com/sai/gRPC/GRpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sai.gRPC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GRpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
