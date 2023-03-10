abe6541099cfa3cefca42e0580e5e4f9d9c027ee
SaiAshish <saiashish3760@gmail.com>
2023-03-11 03:05:36 +0530

    Add files via upload

---
+++ grpc-spring-boot-example/greeting-service/src/main/java/com/sai/grpc/GrpcSpringBootExampleApplication.java
@@ -0,0 +1,13 @@
+package com.sai.grpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcSpringBootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcSpringBootExampleApplication.class, args);
+	}
+
+}
---
+++ grpc-spring-boot-example/greeting-service/src/test/java/com/sai/grpc/GrpcSpringBootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sai.grpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcSpringBootExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
